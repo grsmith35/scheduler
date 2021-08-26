2f257de3f26ef1789e998748cd2c026e8581805c
Riley Smith <gordonrileysmith@gmail.com>
2021-08-26 15:22:02 -0600

    initial structure

---
+++ Develop/js/script.js
@@ -0,0 +1,8 @@
+var currentTime = moment();
+var containerArea = document.querySelector("#container-area");
+
+var changeEvent = function(event) {
+    event.target
+};
+
+containerArea.addEventListener("click", changeEvent)
